316202c54f84ac86a272567f964a15cc4599034b
Olesea <oleseasaitan@gmail.com>
2019-03-15 18:32:26 +0100

    er1

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE HTML>
-<html lang="es">
+<html lang="es" xmlns="http://www.w3.org/1999/html">
     <head>
         <meta charset="utf-8" />
         <title>Tiendas de camisetas</title>
         <link rel="stylesheet" href="assets/css/styles.css">
     </head>
     <body>
+
+
         <!--CABECERA -->
         <header id="header">
             <div id="logo">
@@ -15,27 +17,17 @@
                 </a>
             </div>
         </header>
+
+
         <!-- MENU -->
         <nav id="menu">
             <ul>
-                <li>
-                    <a href="#">Inicio</a>
-                </li>
-                <li>
-                    <a href="#">Categoria 1</a>
-                </li>
-                <li>
-                    <a href="#">Categoria 2</a>
-                </li>
-                <li>
-                    <a href="#">Categoria 3</a>
-                </li>
-                <li>
-                    <a href="#">Categoria 4</a>
-                </li>
-                <li>
-                    <a href="#">Categoria 5</a>
-                </li>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Categoria 1</a></li>
+                <li><a href="#">Categoria 2</a></li>
+                <li><a href="#">Categoria 3</a></li>
+                <li><a href="#">Categoria 4</a></li>
+                <li><a href="#">Categoria 5</a></li>
             </ul>
         </nav>
 
@@ -43,15 +35,16 @@
             <!-- BARRA LATERAL -->
             <aside id="lateral">
                 <div id="login" class="block_aside">
-                <form action="#" method="post">
-                    <label for="email">Email</label>
-                    <input type="email" name="email"/>
+                    <h3>Entrar a la web</h3>
+                     <form action="#" method="post">
+                        <label for="email">Email</label>
+                        <input type="email" name="email"/>
 
-                    <label for="password">Contraseña</label>
-                    <input type="password" name="password"/>
+                        <label for="password">Contraseña</label>
+                        <input type="password" name="password"/>
 
-                    <input type="submit" value="Enviar" />
-                </form>
+                        <input type="submit" value="Enviar" />
+                     </form>
                     <ul>
                         <li> <a href="#">Mis pedidos</a></li>
                         <li> <a href="#">Gestionar pedidos</a></li>
@@ -59,11 +52,40 @@
                     </ul>
                 </div>
             </aside>
+
+
+
+            <!-- CONTENIDO CENTRAL -->
+            <div id ="central">
+                <h1>Productos destacados</h1>
+                <div class="product">
+                    <img src="assets/img/camiseta.png">
+                    <h2>Camiseta Azul Ancha</h2>
+                    <p>30 euros</p>
+                    <a href="" class="button" Comprar</a>
+                </div>
+
+                <div class="product">
+                    <img src="assets/img/camiseta.png">
+                    <h2>Camiseta Azul Ancha</h2>
+                    <p>30 euros</p>
+                    <a href="" class="button" Comprar</a>
+                </div>
+
+                <div class="product">
+                    <img src="assets/img/camiseta.png">
+                    <h2>Camiseta Azul Ancha</h2>
+                    <p>30 euros</p>
+                    <a href="" class="button" Comprar</a>
+                </div>
+            </div>
         </div>
 
+           <!-- PIE DE PAGINA -->
+            <footer id="footer"
+                    <p>Desarollado por Ion Saitan &copy: <?=date('Y')?></p>
+            </footer>
 
-        <!-- CONTENIDO CENTRAL -->
 
-        <!-- PIE DE PAGINA -->
     </body>
 </html>
