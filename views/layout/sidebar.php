76b72f98ccccf52f0ba424a1551938f3e86dba1a
vaniusa <ionsaitan1980@gmail.com>
2019-03-19 22:57:35 +0100

    Guardando una categoria nueva

--- views/layout/sidebar.php
+++ views/layout/sidebar.php
@@ -17,7 +17,7 @@
         <?php endif; ?>
         <ul>
             <?php if (isset($_SESSION['admin'])): ?>
-            <li> <a href="#">Gestionar categorias</a></li>
+            <li> <a href="<?php base_url?>categoria/index">Gestionar categorias</a></li>
             <li> <a href="#">Gestionar productos</a></li>
             <li><a href="#">Gestionar pedidos</a></li>
             <?php endif; ?>
@@ -25,6 +25,8 @@
             <?php if (isset($_SESSION['identity'])): ?>
             <li><a href="#">Mis pedidos</a></li>
             <li><a href="<?=base_url?>usuario/logout">Cerrar session</a></li>
+            <?php else: ?>
+                <li> <a href="<?php base_url?>usuario/registro">Registrate aqui</a></li>
             <?php endif; ?>
         </ul>
     </div>
