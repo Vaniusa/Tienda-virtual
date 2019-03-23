cbc0c195af685105efeb3b7e2609f5d8702fe59a
Olesea <oleseasaitan@gmail.com>
2019-03-23 06:40:58 +0100

    Eliminando un producto

--- controllers/ProductoController.php
+++ controllers/ProductoController.php
@@ -58,12 +58,12 @@ class ProductoController
                 if ($mimetype == "image/jpg" || $mimetype == 'image/jpeg' ||
                     $mimetype == 'image/png' || $mimetype == 'imge/gif') {
 
-                    if (!is_dir('uploads/image')){
-                        mkdir('uploads/images',0777,true);
+                    if (!is_dir('uploads/image')) {
+                        mkdir('uploads/images', 0777, true);
                     }
                     $producto->setImagen($filename);
                     move_uploaded_file($file['tmp_name'],
-                        'uploads/images/'.$filename);
+                        'uploads/images/' . $filename);
 
                 }
 
@@ -84,6 +84,32 @@ class ProductoController
         }
 
         header('Location:' . base_url . 'producto/gestion');
+    }
+
+    public function editar()
+    {
+        var_dump($_GET);
+    }
+
+    public function eliminar()
+    {
+        Utils::isAdmin();
+
+        if (isset($_GET['id'])){
+            $id = $_GET['id'];
+            $producto = new Producto();
+            $producto->setId($id);
+
+            $delete = $producto->delete();
+            if($delete){
+                $_SESSION['delete'] = 'complete';
+            }else{
+                $_SESSION['delete'] = 'failed';
+            }
+        }else{
+            $_SESSION['delete'] = 'failed';
+        }
+        header('Location:'.base_url.'producto/gestion');
     }
 
 }
